03e99c36d85cbaa3c69bf546073c9ef84a08b418
iliikarasev01262011-cyber <iliikarasev01262011@gmail.com>
2026-09-01 22:34:15 +0300

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -46,7 +46,8 @@ def main_menu():
     btn6 = types.InlineKeyboardButton("График", callback_data="grafik")
     btn7 = types.InlineKeyboardButton("📝 Оставить заявку", callback_data="zayavka")
     btn8 = types.InlineKeyboardButton("Связь", url="https://t.me/imkol_official_channel")
-    markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8)
+    btn9 = types.InlineKeyboardButton("Новостной канал", url = "https://t.me/imkol_dev")
+    markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9)
     return markup
 
 # --- Обработчики ---
